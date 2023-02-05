5ef2727359a433f7ec13d722ab0281e0fdac5e9f
Kawsar Bin Siraj <38612699+kawsarBinSiraj@users.noreply.github.com>
2023-02-05 10:21:15 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,30 +93,7 @@ $(document).ready(function () {
         }
     });
 
-
-//     // Add smooth scrolling to all links
-//     $(".menu__inner ul li > a").on('click', function (event) {
-
-//         // Make sure this.hash has a value before overriding default behavior
-//         if (this.hash !== "") {
-//             // Prevent default anchor click behavior
-//             event.preventDefault();
-
-//             // Store hash
-//             var hash = this.hash;
-
-//             // Using jQuery's animate() method to add smooth page scroll
-//             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//             $('html, body').animate({
-//                 scrollTop: $(hash).offset().top
-//             }, 800, function () {
-
-//                 // Add hash (#) to URL when done scrolling (default click behavior)
-//                 window.location.hash = hash;
-//             });
-//         } // End if
-//     });
-
+    
     // back-to-top
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
